e8a607691b7c68e7f3e1529a79353ef90bca8eec
Damien Brockmann <damienbrockmann@gmail.com>
2020-05-07 10:37:43 -0500

    wireframe for new app

---
+++ src/components/NewApp.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../stylesheets/newmain.scss';
+
+const App = () => {
+  return (
+    <div className="accessudemo2020">
+      <div className="controls">
+        <div className="test"/>
+        <div className="test"/>
+        <div className="test"/>
+      </div>
+      <div className="infoBar test" />
+      <div className="voiceText test" />
+      <div className="dnd test">
+        <div className="trainPanel">
+          <div className="trainHeader test" />
+          <div className="trainVideo test" />
+          <div className="trainControls test" />
+        </div>
+      </div>
+      <div className="notification" />
+    </div>);
+}
+
+export default App;
