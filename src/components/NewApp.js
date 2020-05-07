302e1737bcc84b7f69f92d6213eb54a360985cea
Damien Brockmann <damienbrockmann@gmail.com>
2020-05-07 13:16:59 -0500

    file sorting

--- src/components/NewApp.js
+++ src/components/NewApp.js
@@ -1,28 +1,50 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { VoiceConsumer } from './VoiceProvider';
 import InfoBox from '../containers/InfoBox';
 import Controls from './Controls';
+import VoiceProvider from '../containers/VoiceContainer';
+import Notification from '../containers/NotificationContainer';
+import DnDArea from '../containers/DnDAreaContainer';
+import Thing from '../containers/ThingContainer';TrainPanel
+import TrainPanel from '../containers/TrainPanel';
+import store from '../store';
 import '../stylesheets/newmain.scss';
 
 
 const App = () => {
   return (
-    <div className="accessudemo2020">
-      <div className="controls">
-        <div className="test"/>
-        <div className="test"/>
-        <div className="test"/>
-      </div>
-      <InfoBox />
-      <div className="voiceText test" />
-      <div className="dnd test">
-        <div className="trainPanel">
-          <div className="trainHeader test" />
-          <div className="trainVideo test" />
-          <div className="trainControls test" />
+    <Provider store={store}>
+      <VoiceProvider>
+        <div className="accessudemo2020">
+          <Controls />
+          <InfoBox />
+          <div className="voiceText test" />
+          <div className="dnd">
+            <DnDArea>
+              <VoiceConsumer>
+                {voice =>
+                  <Thing type="mouse" voice={voice}/>
+                }
+              </VoiceConsumer>
+              <VoiceConsumer>
+                {voice =>
+                  <Thing type="cat" voice={voice}/>
+                }
+              </VoiceConsumer>
+              <VoiceConsumer>
+                {voice =>
+                  <Thing type="cheese" voice={voice}/>
+                }
+              </VoiceConsumer>
+            </DnDArea>
+            <TrainPanel />
+          </div>
+          <Notification />
         </div>
-      </div>
-      <div className="notification" />
-    </div>);
+      </VoiceProvider>
+    </Provider>
+    );
 }
 
 export default App;
