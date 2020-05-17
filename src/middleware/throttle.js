173a43c435ae8575a8eb9d79af8f1a6bdd673e16
Damien Brockmann <damienbrockmann@gmail.com>
2020-05-17 16:39:04 -0500

    improve freq spectrum

--- src/middleware/throttle.js
+++ src/middleware/throttle.js
@@ -2,7 +2,6 @@ import { UPDATE_NOTIFCATION } from '../actions';
 let timeout;
 let timedOut;
 
-console.log(UPDATE_NOTIFCATION);
 const throttle = store => next => action => {
   if (action.type !== UPDATE_NOTIFCATION) {
     return next(action);
